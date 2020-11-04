48baca0ba749f93a442eae0a3d98fa61a10a2ffa
Milena <milena.dehde@googlemail.com>
2020-11-04 14:45:07 +0100

    Update processCapability.R

--- R/processCapability.R
+++ R/processCapability.R
@@ -16,5 +16,56 @@
 #
 
 processCapability <- function(jaspResults, dataset, options, ...){
+  variables <- unlist(options$diameter)
+  subgroupsName <- options$subgroups
+  makeSubgroups <- subgroupsName != ""
+  
+  if (is.null(dataset)) {
+    if (makeSubgroups) {
+      dataset         <- .readDataSetToEnd(columns.as.numeric = variables, columns.as.factor = subgroupsName)
+      dataset.factors <- .readDataSetToEnd(columns = variables, columns.as.factor = subgroupsName)
+    } else {
+      dataset         <- .readDataSetToEnd(columns.as.numeric = variables)
+      dataset.factors <- .readDataSetToEnd(columns = variables)
+    }
+  }
+
+  # Initial Process Capability Study
+  
+    # X-bar chart (by Tom)
+    if (options$initialControlchart) {
+      
+    }
+    # Histogram (by Jonas)
+    if (options$initialHistogram) {
+     
+    }
+    # Normal Probability Plot
+    if (options$initialProbabilityPlot) {
+    
+    }
+    # Process Capability of Diameter (implement myself)
+    if (options$initialCapabilityAnalysis) {
+    
+    }
+  
+  # Follow-up Process Capability Study
+  
+    # X-bar & Range Control Chart (by Tom)
+    if (options$followupControlchart) {
+    
+    }
+    # Histogram (by Jonas)
+    if (options$followupHistogram) {
+    
+    }
+    # Normal Probability Plot
+    if (options$followupProbabilityPlot) {
+    
+    }
+    # Process Capability of Diameter (implement myself)
+    if (options$followupCapabilityAnalysis) {
+    
+    }
   
 }
