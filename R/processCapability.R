1827517683e37ae822ad79119c65d76e2ed531e9
Milena <milena.dehde@googlemail.com>
2020-11-05 17:01:23 +0100

    Update processCapability.R

--- R/processCapability.R
+++ R/processCapability.R
@@ -15,7 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-processCapability <- function(jaspResults, dataset, options, ...){
+processCapability <- function(jaspResults, dataset, options){
+  
   variables <- unlist(options$diameter)
   subgroupsName <- options$subgroups
   makeSubgroups <- subgroupsName != ""
@@ -30,42 +31,80 @@ processCapability <- function(jaspResults, dataset, options, ...){
     }
   }
 
-  # Initial Process Capability Study
+# Initial Process Capability Study
   
-    # X-bar chart (by Tom)
-    if (options$initialControlchart) {
+  # X-bar chart (by Tom)
+  if (options$initialControlchart) {
       
-    }
-    # Histogram (by Jonas)
-    if (options$initialHistogram) {
+  }
+  # Histogram (by Jonas)
+  if (options$initialHistogram) {
      
-    }
-    # Normal Probability Plot
-    if (options$initialProbabilityPlot) {
+  }
+  # Normal Probability Plot
+  if (options$initialProbabilityPlot) {
     
+  }
+    # Process Capability of Diameter (by Milena)
+  if (options$initialCapabilityAnalysis && is.null(jaspResults[["initialCapabilityAnalysis"]])){
+    if(is.null(jaspResults[["initialCapabilityAnalysis"]])) {
+      jaspResults[["initialCapabilityAnalysis"]] <- createJaspContainer(gettext("Process Capability of Diameter (Initial Capability Study)"))
+      jaspResults[["initialCapabilityAnalysis"]]$dependOn(c("initialCapabilityAnalysis","diameter","subgroups"))
+      jaspResults[["initialCapabilityAnalysis"]]$position <- 4
     }
-    # Process Capability of Diameter (implement myself)
-    if (options$initialCapabilityAnalysis) {
     
-    }
+    initialCapabilityAnalysis <- jaspResults[["initialCapabilityAnalysis"]]
+    
+    initialCapabilityAnalysis[["processDataTable"]] <- .processDataTable(options, dataset)
+    
+  }
   
-  # Follow-up Process Capability Study
+# Follow-up Process Capability Study
   
-    # X-bar & Range Control Chart (by Tom)
-    if (options$followupControlchart) {
+  # X-bar & Range Control Chart (by Tom)
+  if (options$followupControlchart) {
     
-    }
-    # Histogram (by Jonas)
-    if (options$followupHistogram) {
+  }
+  # Histogram (by Jonas)
+  if (options$followupHistogram) {
     
-    }
-    # Normal Probability Plot
-    if (options$followupProbabilityPlot) {
+  }
+  # Normal Probability Plot
+  if (options$followupProbabilityPlot) {
     
-    }
-    # Process Capability of Diameter (implement myself)
-    if (options$followupCapabilityAnalysis) {
+  }
+  # Process Capability of Diameter (by Milena)
+  if (options$followupCapabilityAnalysis) {
     
-    }
-  
+  }
+  return()
 }
+
+.processDataTable <- function(options, dataset){
+  
+  processDataTable <- createJaspTable(title = gettext("Process Data"))
+  processDataTable$position <- 1
+
+  processDataTable$addColumnInfo(name = "lowerSpecificationLimit", type = "number", title = gettext("LSL"))
+  processDataTable$addColumnInfo(name = "targetValue", type = "number", title = gettext("Target"))
+  processDataTable$addColumnInfo(name = "upperSpecificationLimit", type = "number", title = gettext("USL"))
+  processDataTable$addColumnInfo(name = "sampleMean", type = "number", title = gettext("Sample Mean"))
+  processDataTable$addColumnInfo(name = "sampleN", type = "number", title = gettext("Sample N"))
+  processDataTable$addColumnInfo(name = "stDevWithin", type = "number", title = gettext("StDev (Within)"))
+  processDataTable$addColumnInfo(name = "stDevOverall", type = "number", title = gettext("StDev (Overall)"))
+  
+  upperSpecificationLimit <- options$upperSpecification
+  lowerSpecificationLimit <- options$lowerSpecification
+  targetValue <- options$targetValue
+  sampleMean <- mean(options$diameter)
+  sampleN <- length(options$diameter)
+
+  stDevWithin <- numeric()
+  stDevOverall <- sd(options$diameter)
+
+  
+  processDataTable$addRows(list(lowerSpecificationLimit,targetValue,upperSpecificationLimit,sampleMean,sampleN,stDevWithin,stDevOverall))
+    
+  return(processDataTable)
+}
+  
