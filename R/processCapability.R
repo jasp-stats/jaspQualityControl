71e991baf7c65b335a39777303fc92a862d7ed0a
Milena <milena.dehde@googlemail.com>
2020-11-06 16:43:59 +0100

    Update processCapability.R

--- R/processCapability.R
+++ R/processCapability.R
@@ -54,8 +54,8 @@ processCapability <- function(jaspResults, dataset, options){
     }
     
     initialCapabilityAnalysis <- jaspResults[["initialCapabilityAnalysis"]]
-    
     initialCapabilityAnalysis[["processDataTable"]] <- .processDataTable(options, dataset)
+    
     
   }
   
@@ -74,37 +74,56 @@ processCapability <- function(jaspResults, dataset, options){
     
   }
   # Process Capability of Diameter (by Milena)
-  if (options$followupCapabilityAnalysis) {
+  if (options$followupCapabilityAnalysis && is.null(jaspResults[["followupCapabilityAnalysis"]])){
+    if(is.null(jaspResults[["followupCapabilityAnalysis"]])) {
+      jaspResults[["followupCapabilityAnalysis"]] <- createJaspContainer(gettext("Process Capability of Diameter (Follow-up Capability Study)"))
+      jaspResults[["followupCapabilityAnalysis"]]$dependOn(c("followupCapabilityAnalysis","diameter","subgroups"))
+      jaspResults[["followupCapabilityAnalysis"]]$position <- 8
+    }
     
+    followupCapabilityAnalysis <- jaspResults[["followupCapabilityAnalysis"]]
+    followupCapabilityAnalysis[["processDataTable"]] <- .processDataTable(options, dataset)
   }
+  
   return()
 }
-
+  
 .processDataTable <- function(options, dataset){
   
   processDataTable <- createJaspTable(title = gettext("Process Data"))
-  processDataTable$position <- 1
+  processDataTable$dependOn(c("upperSpecification","lowerSpecification","targetValue","diameter","subgroups"))
 
-  processDataTable$addColumnInfo(name = "lowerSpecificationLimit", type = "number", title = gettext("LSL"))
-  processDataTable$addColumnInfo(name = "targetValue", type = "number", title = gettext("Target"))
-  processDataTable$addColumnInfo(name = "upperSpecificationLimit", type = "number", title = gettext("USL"))
-  processDataTable$addColumnInfo(name = "sampleMean", type = "number", title = gettext("Sample Mean"))
-  processDataTable$addColumnInfo(name = "sampleN", type = "number", title = gettext("Sample N"))
-  processDataTable$addColumnInfo(name = "stDevWithin", type = "number", title = gettext("StDev (Within)"))
-  processDataTable$addColumnInfo(name = "stDevOverall", type = "number", title = gettext("StDev (Overall)"))
+  
+  processDataTable$addColumnInfo(name = "lowerSpecificationLimit", type = "number", title = gettext("LSL"), format = "dp:")
+  processDataTable$addColumnInfo(name = "targetValue", type = "number", title = gettext("Target"), format = "dp:0")
+  processDataTable$addColumnInfo(name = "upperSpecificationLimit", type = "number", title = gettext("USL"), format = "dp:0")
+  processDataTable$addColumnInfo(name = "sampleMean", type = "number", title = gettext("Sample Mean"), format = "dp:4")
+  processDataTable$addColumnInfo(name = "sampleN", type = "number", title = gettext("Sample N"), format = "dp:0")
+  processDataTable$addColumnInfo(name = "stDevWithin", type = "number", title = gettext("StDev (Within)"), format = "dp:4")
+  processDataTable$addColumnInfo(name = "stDevOverall", type = "number", title = gettext("StDev (Overall)"), format = "dp:4")
   
   upperSpecificationLimit <- options$upperSpecification
   lowerSpecificationLimit <- options$lowerSpecification
   targetValue <- options$targetValue
-  sampleMean <- mean(options$diameter)
-  sampleN <- length(options$diameter)
-
+  sampleMean <- numeric()
+  sampleN <- numeric()
+  
   stDevWithin <- numeric()
   stDevOverall <- sd(options$diameter)
+  
+  processDataTable$addRows(list("lowerSpecificationLimit" = lowerSpecificationLimit,
+                                "targetValue" = targetValue,
+                                "upperSpecificationLimit" = upperSpecificationLimit,
+                                "sampleMean" = sampleMean,
+                                "sampleN" = sampleN,
+                                "stDevWithin" = stDevWithin,
+                                "stDevOverall" = stDevOverall))
+  
+  return(processDataTable)
+  
+}
+
 
   
-  processDataTable$addRows(list(lowerSpecificationLimit,targetValue,upperSpecificationLimit,sampleMean,sampleN,stDevWithin,stDevOverall))
-    
-  return(processDataTable)
-}
+
   
