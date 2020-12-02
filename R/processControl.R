73bd0a9289ffc4c7d7b202d832f7b8bf87d5a5a8
tomishay <71082046+tomishay@users.noreply.github.com>
2020-12-02 11:34:17 +0100

    Update processControl.R

--- R/processControl.R
+++ R/processControl.R
@@ -16,13 +16,13 @@
 #
 
 processControl <- function(jaspResults, dataset, options){
-
+  
   # Make the Process ID and clean the dataset
   variables <- unlist(options$variables)
   ID <- options$ProcessID
   makeID <- ID != ""
   numberMissing <- 0
-
+  
   if (is.null(dataset)) {
     if (makeID) {
       dataset         <- .readDataSetToEnd(columns.as.numeric = variables, columns.as.factor = ID)
@@ -41,73 +41,75 @@ processControl <- function(jaspResults, dataset, options){
     dataset <- na.omit(dataset)
   } else {dataset <- na.omit(dataset)
   }
-
+  
   .SchartNoId <- function(dataset, options) {
-
+    
     ready <- (length(options$variables) > 1)
     if (!ready)
       return()
-
+    
     data1 <- dataset
     Stdv <- apply(data1, 1, function(x) sd(x))
     subgroups <- 1:length(Stdv)
-    data2 <- data.frame(subgroups = subgroups, Stdv = Stdv)
+    data_plot <- data.frame(subgroups = subgroups, Stdv = Stdv)
     sixsigma <- qcc::qcc(data1, type ='S', plot=FALSE)
     center <- sixsigma$center
     UCL <- max(sixsigma$limits)
     LCL <- min(sixsigma$limits)
+    yBreaks <- jaspGraphs::getPrettyAxisBreaks(c(LCL-5, UCL+5, data_plot$Stdv))
+    yLimits <- range(yBreaks)
+    xBreaks <- jaspGraphs::getPrettyAxisBreaks(c(subgroups))
+    xLimits <- range(xBreaks)
     dfLabel <- data.frame(
-      x = length(subgroups) + 1.2,
+      x = max(xLimits) + 1,
       y = c(center, UCL, LCL),
       l = c(
-        gettextf("Mean = %g", round(center, 3)),
+        gettextf("SD = %g", round(center, 3)),
         gettextf("UCL = %g",   round(UCL, 3)),
         gettextf("LCL = %g",   round(LCL, 3))
       )
     )
-    yBreaks <- jaspGraphs::getPrettyAxisBreaks(c(LCL-10, UCL+10))
-    yLimits <- range(yBreaks)
-    xBreaks <- jaspGraphs::getPrettyAxisBreaks(c(subgroups))
-
-    p <- ggplot2::ggplot(data2, ggplot2::aes(x = subgroups, y = Stdv)) +
-      ggplot2::geom_line() +
-      ggplot2::geom_point(size = 2, col = ifelse(data2$Stdv > UCL | data2$Stdv < LCL, 2, 1)) +
+    
+    
+    p <- ggplot2::ggplot(data_plot, ggplot2::aes(x = subgroups, y = Stdv)) +
+      jaspGraphs::geom_line() +
+      jaspGraphs::geom_point(size = 4, fill = ifelse(data_plot$Stdv > UCL | data_plot$Stdv < LCL, 'red', 'gray')) +
       ggplot2::geom_hline(yintercept =  center, color = 'black') +
       ggplot2::geom_hline(yintercept = c(UCL, LCL), color = "red") +
       ggplot2::geom_label(data = dfLabel, mapping = ggplot2::aes(x = x, y = y, label = l),inherit.aes = FALSE) +
       ggplot2::scale_y_continuous(name = "Subgroup Standard Deviation" ,limits = yLimits, breaks = yBreaks) +
-      ggplot2::scale_x_continuous(name = 'Subgroup', breaks = xBreaks, limits = c(1, length(subgroups) + 1.5)) +
+      ggplot2::scale_x_continuous(name = 'Subgroup', breaks = xBreaks, limits = xLimits + 1) +
       jaspGraphs::geom_rangeframe() +
       jaspGraphs::themeJaspRaw()
-
+    
     return(p)
   }
-
+  
   #X bar R Chart
   if(options$XbarRchart && is.null(jaspResults[["XbarRchart"]])){
-
+    
     jaspResults[["XbarPlot"]] <- createJaspPlot(title = "X bar chart", width = 1100, height = 400)
     jaspResults[["XbarPlot"]]$dependOn(c("XbarRchart"))
     jaspResults[["XbarPlot"]]$position <- 11
     XbarPlot <- jaspResults[["XbarPlot"]]
     XbarPlot$plotObject <- .XbarchartNoId(dataset = dataset, options = options)
-
+    
     jaspResults[["RPlot"]] <- createJaspPlot(title = "R chart", width = 1100, height= 400)
     jaspResults[["RPlot"]]$dependOn(c("XbarRchart"))
     jaspResults[["RPlot"]]$position <- 11
     RPlot<- jaspResults[["RPlot"]]
     RPlot$plotObject <- .RchartNoId(dataset = dataset, options = options)
   }
-
+  
   #X bar S Chart
   if(options$Xbarschart && is.null(jaspResults[["Xbarschart"]])){
-
+    
     jaspResults[["XbarPlot"]] <- createJaspPlot(title = "X bar chart", width = 1100, height = 400)
     jaspResults[["XbarPlot"]]$dependOn(c("Xbarschart"))
     jaspResults[["XbarPlot"]]$position <- 11
     XbarPlot <- jaspResults[["XbarPlot"]]
     XbarPlot$plotObject <- .XbarchartNoId(dataset = dataset, options = options)
-
+    
     jaspResults[["SPlot"]] <- createJaspPlot(title = "S chart", width = 1100, height= 400)
     jaspResults[["SPlot"]]$dependOn(c("Xbarschart"))
     jaspResults[["SPlot"]]$position <- 11
