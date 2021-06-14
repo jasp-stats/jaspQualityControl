7cd7439da822e553af115546cd6fc29fb87687ad
Koen Derks <koen-derks@hotmail.com>
2021-06-14 17:16:28 +0200

    Update unit tests (#102)

--- tests/testthat/test-processCapabilityStudies.R
+++ tests/testthat/test-processCapabilityStudies.R
@@ -1,27 +1,15 @@
 context("[Quality Control] Process Capability Studies")
 
 options <- analysisOptions("processCapabilityStudies")
-options$capabilityStudy <- "initialCapabilityAnalysis"
-options$lowerSpecification <- 4
+options$capabilityStudyType <- "normalCapabilityAnalysis"
+options$controlChartsType <- "xbarR"
 options$lowerSpecificationField <- TRUE
-options$nonNormalCapabilityStudy <- TRUE
-options$nullDistribution <- "Lognormal"
 options$rank <- "Bernard"
-options$targetValue <- 8
-options$targetValueField <- TRUE
-options$upperSpecification <- 12
 options$upperSpecificationField <- TRUE
-options$variables <- c("Measurement1", "Measurement2", "Measurement3")
+options$variablesLong <- "Measurement1"
 set.seed(1)
-results <- runAnalysis("processCapabilityStudies", "partOperatorData.csv", options)
+results <- runAnalysis("processCapabilityStudies", "C:/Users/derksk/OneDrive - NBU/Desktop/PhD/JASP modules/jaspProcessControl/tests/testthat/partOperatorData.csv", options)
 
-
-test_that("Process Capability based on the Weibull Distribution table results match", {
-	table <- results[["results"]][["capabilityAnalysis"]][["collection"]][["capabilityAnalysis_nonNormalCapabilityAnalysis"]][["collection"]][["capabilityAnalysis_nonNormalCapabilityAnalysis_capabilityTableNonNormal"]][["data"]]
-	jaspTools::expect_equal_tables(table,
-		list(4.94218019580985, 4, 8.07925471611111, 1.87033619812937, 8.80523617115434,
-			 12))
-})
 
 test_that("Capability of the Process plot matches", {
 	plotName <- results[["results"]][["capabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis_capabilityPlot"]][["data"]]
@@ -29,69 +17,59 @@ test_that("Capability of the Process plot matches", {
 	jaspTools::expect_equal_plots(testPlot, "capability-of-the-process", dir="processCapabilityStudies")
 })
 
-test_that("Process Performance (Total) table results match", {
+test_that("Overall Capability table results match", {
 	table <- results[["results"]][["capabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis_capabilityTableOverall"]][["data"]]
 	jaspTools::expect_equal_tables(table,
-		list(0.712884311743994, 0.698759450807161, 0.727009172680828, 1.36617298250936,
-			 0.698759450807161))
+		list(0.171532676484168, -1.19359203285214, -1.52311578649644, -0.864068279207846,
+			 1.53665738582048, 0.127598556598227, -1.19359203285214, 0.215383203520269
+			))
 })
 
-test_that("Process Capability (Within) table results match", {
+test_that("Performance table results match", {
+	table <- results[["results"]][["capabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis_capabilityTablePerformance"]][["data"]]
+	jaspTools::expect_equal_tables(table,
+		list(2.01361451190873, 16.9211440957939, 0, "PPM &lt; LSL", 0.00247021703092543,
+			 374.780947267839, 999828.712435699, 999359.808753505, 1e+06,
+			 "PPM &gt; USL", 995231.851356578, 999997.553773326, 999830.726050211,
+			 999376.7298976, 1e+06, "PPM total", "", ""))
+})
+
+test_that("Potential Capability (Within) table results match", {
 	table <- results[["results"]][["capabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis_capabilityTableWithin"]][["data"]]
 	jaspTools::expect_equal_tables(table,
-		list(1.37070002254134, 1.34354141225133, 1.39785863283134, 1.34354141225133,
-			 4.030624236754))
+		list(0.154266470540219, -1.07344696035231, -1.41504931744125, -0.731844603263367,
+			 1.38197990143274, 0.108585346037329, -1.07344696035231, 0.199877344433774,
+			 -3.22034088105692))
 })
 
 test_that("Process Summary table results match", {
 	table <- results[["results"]][["capabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis"]][["collection"]][["capabilityAnalysis_normalCapabilityAnalysis_processSummaryTable"]][["data"]]
 	jaspTools::expect_equal_tables(table,
-		list(4, 8.07925471611111, 30, 1.87033619812937, 0.972738973813743,
-			 8, 12))
+		list(-1, 7.95839450136667, 30, 1.94326433986529, 2.16076333480663,
+			 1))
 })
 
-test_that("R Chart plot matches", {
-	plotName <- results[["results"]][["controlCharts"]][["collection"]][["controlCharts_rplot"]][["data"]]
+test_that("X-bar & R Chart plot matches", {
+	plotName <- results[["results"]][["controlCharts"]][["collection"]][["controlCharts_plot"]][["data"]]
 	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-	jaspTools::expect_equal_plots(testPlot, "r-chart", dir="processCapabilityStudies")
+	jaspTools::expect_equal_plots(testPlot, "x-bar-r-chart", dir="processCapabilityStudies")
 })
 
-test_that("X-bar Chart plot matches", {
-	plotName <- results[["results"]][["controlCharts"]][["collection"]][["controlCharts_xplot"]][["data"]]
-	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-	jaspTools::expect_equal_plots(testPlot, "x-bar-chart", dir="processCapabilityStudies")
-})
-
-test_that("Distribution Plot matches", {
+test_that("Histogram plot matches", {
 	plotName <- results[["results"]][["histogram"]][["data"]]
 	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-	jaspTools::expect_equal_plots(testPlot, "distribution-plot", dir="processCapabilityStudies")
+	jaspTools::expect_equal_plots(testPlot, "histogram", dir="processCapabilityStudies")
 })
 
-test_that("Measurement1 plot matches", {
-	plotName <- results[["results"]][["probabilityContainer"]][["collection"]][["probabilityContainer_plotContainer"]][["collection"]][["probabilityContainer_plotContainer_Measurement1"]][["data"]]
+test_that("Probability Plot matches", {
+	plotName <- results[["results"]][["probabilityContainer"]][["collection"]][["probabilityContainer_ProbabilityPlot"]][["data"]]
 	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-	jaspTools::expect_equal_plots(testPlot, "measurement1", dir="processCapabilityStudies")
+	jaspTools::expect_equal_plots(testPlot, "probability-plot", dir="processCapabilityStudies")
 })
 
-test_that("Measurement2 plot matches", {
-	plotName <- results[["results"]][["probabilityContainer"]][["collection"]][["probabilityContainer_plotContainer"]][["collection"]][["probabilityContainer_plotContainer_Measurement2"]][["data"]]
-	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-	jaspTools::expect_equal_plots(testPlot, "measurement2", dir="processCapabilityStudies")
-})
-
-test_that("Measurement3 plot matches", {
-	plotName <- results[["results"]][["probabilityContainer"]][["collection"]][["probabilityContainer_plotContainer"]][["collection"]][["probabilityContainer_plotContainer_Measurement3"]][["data"]]
-	testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-	jaspTools::expect_equal_plots(testPlot, "measurement3", dir="processCapabilityStudies")
-})
-
-test_that("Summary of Tests Against the Lognormal Distribution table results match", {
+test_that("Summary of test against the Normal distribution table results match", {
 	table <- results[["results"]][["probabilityContainer"]][["collection"]][["probabilityContainer_probabilityTable"]][["data"]]
 	jaspTools::expect_equal_tables(table,
-		list(0.508725540645823, 2.04619141068134, 30, 0.73653710866348, "No",
-			 0.235586590929892, "Measurement1", 0.3362181689652, 2.04342958808724,
-			 30, 0.907985736560375, "No", 0.217214387143492, "Measurement2",
-			 0.852722059928624, 2.0971825233772, 30, 0.443502084476377, "No",
-			 0.245886916677861, "Measurement3"))
+		list(0.727071326290741, 7.95839450136667, 30, 0.535382300681328, 1.94326433986529
+			))
 })
