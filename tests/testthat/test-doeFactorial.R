8e1df0216e913cffaa60bbc1deebb1c3e9e6e9d9
Tom <tomishay2000@gmail.com>
2022-02-22 11:09:23 +0100

    Uni tests fix

--- tests/testthat/test-doeFactorial.R
+++ tests/testthat/test-doeFactorial.R
@@ -1,18 +1,3 @@
-context("DoE factorialAnalysis")
-
-options <- analysisOptions("doeFactorial")
-options$FAresponse <- "Yield"
-options$FAassignedFactors <- c("Aperture.setting" ,"Exposure.time", "Develop.time" ,   "Mask.dimension",  "Etch.time")
-options$FArunOrder <- "RunOrder"
-options$modelTerms <- list(
-  list(components="Aperture.setting"),
-  list(components="Exposure.time"),
-  list(components="Develop.time"),
-  list(components="Mask.dimension"),
-  list(components="Etch.time"))
-
-results <- runAnalysis("doeFactorial", read.csv("doeFactorialAnalysis.csv"), options)
-
 
 
 
