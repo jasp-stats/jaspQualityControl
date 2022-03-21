62fb605d162d772db865c43d2d3618745ed56f09
Tom <tomishay2000@gmail.com>
2022-03-21 13:35:55 +0100

    Update test-attributesCharts.R

--- tests/testthat/test-attributesCharts.R
+++ tests/testthat/test-attributesCharts.R
@@ -23,33 +23,12 @@ test_that("Test results for np chart table results match", {
                                  ))
 })
 
-# P
-options$timeStamp <- "ï..Month"
-options$TypeDefectives <- "pchart"
-results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
-
-test_that("p Chart plot matches", {
-  plotName <- results[["results"]][["PchartPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "p-chart")
-})
-
-# Laney's P
-options$TypeDefectives <- "Laneyprimechart"
-results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
-
-test_that("Laney p' Chart plot matches", {
-  plotName <- results[["results"]][["LaneyPPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "laney-p-chart")
-})
 
 ## Defects
 # C
 options$TypeDefectives <- "npchart"
 options$Attributes <- "Defects"
 options$TypeDefects <- "cchart"
-options$timeStamp <- ""
 results <- runAnalysis("attributesCharts", "SPC_NP.csv", options)
 
 test_that("c Chart plot matches", {
@@ -65,31 +44,80 @@ test_that("Test results for c chart table results match", {
 })
 
 # U
-options$timeStamp <- "ï..Month"
-options$TypeDefects <- "uchart"
-results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
-
 test_that("u Chart plot matches", {
+  options <- analysisOptions("attributesCharts")
+  options$D <- "D"
+  options$total <- "Size"
+  options$timeStamp <- "ï..Month"
+  options$Attributes <- "Defects"
+  options$TypeDefectives <- "npchart"
+  options$TypeDefects <- "uchart"
+  set.seed(1)
+  results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
+  plotName <- results[["results"]][["UchartPlot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "u-chart")
+})
+
+# P
+test_that("u Chart plot matches", {
+  options <- analysisOptions("attributesCharts")
+  options$D <- "D"
+  options$total <- "Size"
+  options$timeStamp <- "ï..Month"
+  options$Attributes <- "Defects"
+  options$TypeDefectives <- "pchart"
+  options$TypeDefects <- "uchart"
+  set.seed(1)
+  results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
+  plotName <- results[["results"]][["UchartPlot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "u-chart")
+})
+
+# Laney's P
+test_that("u Chart plot matches", {
+  options <- analysisOptions("attributesCharts")
+  options$D <- "D"
+  options$total <- "Size"
+  options$timeStamp <- "ï..Month"
+  options$Attributes <- "Defects"
+  options$TypeDefectives <- "Laneyprimechart"
+  options$TypeDefects <- "uchart"
+  set.seed(1)
+  results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
   plotName <- results[["results"]][["UchartPlot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   jaspTools::expect_equal_plots(testPlot, "u-chart")
 })
 
 # Laneys U
-options$TypeDefects <- "Laneychart"
-results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
-
 test_that("Laney u' Chart plot matches", {
+  options <- analysisOptions("attributesCharts")
+  options$D <- "D"
+  options$total <- "Size"
+  options$timeStamp <- "ï..Month"
+  options$Attributes <- "Defects"
+  options$TypeDefectives <- "Laneyprimechart"
+  options$TypeDefects <- "Laneychart"
+  set.seed(1)
+  results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
   plotName <- results[["results"]][["LaneyUPlot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   jaspTools::expect_equal_plots(testPlot, "laney-u-chart")
 })
 
 ## I MR
-options$Attributes <- "ImR"
-results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
-
 test_that("Individuals and Moving Range Chart plot matches", {
+  options <- analysisOptions("attributesCharts")
+  options$D <- "D"
+  options$total <- "Size"
+  options$timeStamp <- "ï..Month"
+  options$Attributes <- "ImR"
+  options$TypeDefectives <- "Laneyprimechart"
+  options$TypeDefects <- "Laneychart"
+  set.seed(1)
+  results <- runAnalysis("attributesCharts", "SPC_P.csv", options)
   plotName <- results[["results"]][["IPlotA"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   jaspTools::expect_equal_plots(testPlot, "individuals-and-moving-range-chart")
