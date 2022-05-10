5935f2dd48c589f553230c42bd649d0160664100
JTPetter <61797391+JTPetter@users.noreply.github.com>
2022-05-09 18:02:36 -0700

    Replace decimal places function in other places

--- R/processCapabilityStudies.R
+++ R/processCapabilityStudies.R
@@ -255,10 +255,14 @@ processCapabilityStudies <- function(jaspResults, dataset, options) {
   table$addRows(rows)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   N.Decimals <- max(sapply(unlist(dataset[measurements]), .decimalplaces))
 =======
   N.Decimals <- max(sapply(unlist(dataset[measurements]), .decimalplaces)) #.decimalplaces(dataset[measurements][[1]][1])
 >>>>>>> 2964ac0 (Fix decimal place calculation across data)
+=======
+  N.Decimals <- max(sapply(unlist(dataset[measurements]), .decimalplaces))
+>>>>>>> 5ba473b (Replace decimal places function in other places)
   if(returnDataframe){
     sourceVector <- c('LSL', 'Target', 'USL', 'Sample size', 'Mean', "Std. Deviation (Total)", "Std. Deviation (Within)")
     lsl <- options[["lowerSpecification"]]
@@ -679,10 +683,14 @@ processCapabilityStudies <- function(jaspResults, dataset, options) {
   expWithin <- c(ewLSL, ewUSL, ewTOT)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   N.Decimals <- max(sapply(unlist(dataset[measurements]), .decimalplaces))
 =======
   N.Decimals <- max(sapply(unlist(dataset[measurements]), .decimalplaces)) #.decimalplaces(dataset[measurements][[1]][1])
 >>>>>>> 2964ac0 (Fix decimal place calculation across data)
+=======
+  N.Decimals <- max(sapply(unlist(dataset[measurements]), .decimalplaces))
+>>>>>>> 5ba473b (Replace decimal places function in other places)
   if(returnPerformanceDataframe){
     df <- data.frame("Source" = rowNames,
                      "Observed" = observed,
