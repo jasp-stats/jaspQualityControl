e7f14fe2e2156e049feb722a7db0c6062a0b5b30
Koen Derks <koen-derks@hotmail.com>
2023-01-18 18:00:40 +0100

    Update tests

--- tests/testthat/test-doeAnalysis.R
+++ tests/testthat/test-doeAnalysis.R
@@ -12,47 +12,7 @@ options$modelTerms <- list(list(components = "Nozzle"), list(components = "Speed
     list(components = c("Nozzle", "Speed_coded", "Pressure_coded"
     )))
 set.seed(1)
-dataset <- structure(list(Run.order = c(19L, 15L, 11L, 6L, 21L, 20L, 10L, 
-22L, 27L, 1L, 5L, 4L, 12L, 2L, 25L, 18L, 8L, 13L, 16L, 14L, 17L, 
-23L, 24L, 3L, 9L, 7L, 26L, 45L, 35L, 50L, 52L, 38L, 29L, 37L, 
-53L, 46L, 41L, 49L, 54L, 43L, 51L, 40L, 48L, 36L, 39L, 42L, 28L, 
-47L, 33L, 32L, 31L, 44L, 34L, 30L), Standard.order = c(1L, 2L, 
-3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 
-17L, 18L, 19L, 20L, 21L, 22L, 23L, 24L, 25L, 26L, 27L, 1L, 2L, 
-3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 
-17L, 18L, 19L, 20L, 21L, 22L, 23L, 24L, 25L, 26L, 27L), Nozzle = c(1L, 
-2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 
-3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 
-1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 1L, 
-2L, 3L, 1L, 2L, 3L), Speed = c(100L, 100L, 100L, 120L, 120L, 
-120L, 140L, 140L, 140L, 100L, 100L, 100L, 120L, 120L, 120L, 140L, 
-140L, 140L, 100L, 100L, 100L, 120L, 120L, 120L, 140L, 140L, 140L, 
-100L, 100L, 100L, 120L, 120L, 120L, 140L, 140L, 140L, 100L, 100L, 
-100L, 120L, 120L, 120L, 140L, 140L, 140L, 100L, 100L, 100L, 120L, 
-120L, 120L, 140L, 140L, 140L), Speed_coded = c(-1L, -1L, -1L, 
-0L, 0L, 0L, 1L, 1L, 1L, -1L, -1L, -1L, 0L, 0L, 0L, 1L, 1L, 1L, 
--1L, -1L, -1L, 0L, 0L, 0L, 1L, 1L, 1L, -1L, -1L, -1L, 0L, 0L, 
-0L, 1L, 1L, 1L, -1L, -1L, -1L, 0L, 0L, 0L, 1L, 1L, 1L, -1L, -1L, 
--1L, 0L, 0L, 0L, 1L, 1L, 1L), Pressure = c(10L, 10L, 10L, 10L, 
-10L, 10L, 10L, 10L, 10L, 15L, 15L, 15L, 15L, 15L, 15L, 15L, 15L, 
-15L, 20L, 20L, 20L, 20L, 20L, 20L, 20L, 20L, 20L, 10L, 10L, 10L, 
-10L, 10L, 10L, 10L, 10L, 10L, 15L, 15L, 15L, 15L, 15L, 15L, 15L, 
-15L, 15L, 20L, 20L, 20L, 20L, 20L, 20L, 20L, 20L, 20L), Pressure_coded = c(-1L, 
--1L, -1L, -1L, -1L, -1L, -1L, -1L, -1L, 0L, 0L, 0L, 0L, 0L, 0L, 
-0L, 0L, 0L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, -1L, -1L, -1L, 
--1L, -1L, -1L, -1L, -1L, -1L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 
-0L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L), Blocks = c(1L, 1L, 1L, 
-1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
-1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 
-2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 
-2L, 2L, 2L), Syruploss = c(-35L, 17L, -39L, -45L, -65L, -55L, 
--40L, 20L, 15L, 110L, 55L, 90L, -10L, -55L, -28L, 80L, 110L, 
-110L, 4L, -23L, -30L, -40L, -64L, -61L, 31L, -20L, 54L, -25L, 
-24L, -35L, -60L, -58L, -67L, 15L, 4L, -30L, 75L, 120L, 113L, 
-30L, -44L, -26L, 54L, 44L, 135L, 5L, -5L, -55L, -30L, -62L, -52L, 
-36L, -31L, 4L)), class = "data.frame", row.names = c(NA, -54L
-))
-results <- runAnalysis("doeAnalysis", dataset, options)
+results <- runAnalysis("doeAnalysis", "syruploss.csv", options)
 
 test_that("Model Summary table results match", {
 	table <- results[["results"]][["tableSummary"]][["data"]]
@@ -94,19 +54,7 @@ options$modelTerms <- list(list(components = "A"), list(components = "B"), list(
     "C", "D")), list(components = c("B", "C", "D")), list(components = c("A", 
     "B", "C", "D")))
 set.seed(1)
-dataset <- structure(list(StdOrder = c(15L, 3L, 2L, 5L, 9L, 8L, 7L, 10L, 
-14L, 11L, 12L, 13L, 4L, 1L, 6L, 16L), RunOrder = 1:16, CenterPt = c(1L, 
-1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L), 
-    Blocks = c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
-    1L, 1L, 1L, 1L), A = c(-1L, -1L, 1L, -1L, -1L, 1L, -1L, 1L, 
-    1L, -1L, 1L, -1L, 1L, -1L, 1L, 1L), B = c(1L, 1L, -1L, -1L, 
-    -1L, 1L, 1L, -1L, -1L, 1L, 1L, -1L, 1L, -1L, -1L, 1L), C = c(1L, 
-    -1L, -1L, 1L, -1L, 1L, 1L, -1L, 1L, -1L, -1L, 1L, -1L, -1L, 
-    1L, 1L), D = c(1L, -1L, -1L, -1L, 1L, -1L, -1L, 1L, 1L, 1L, 
-    1L, 1L, -1L, -1L, -1L, 1L), Response = c(0.7, 0.5, 0.3, 0.2, 
-    0.5, 0.6, 0.4, 0.9, 0.6, 0.5, 0.7, 0.4, 0.3, 0.2, 0.6, 0.7
-    )), class = "data.frame", row.names = c(NA, -16L))
-results <- runAnalysis("doeAnalysis", dataset, options)
+results <- runAnalysis("doeAnalysis", "minitab.csv", options)
 
 
 test_that("ANOVA table results match", {
