f051716e0b1b57dddf431a5a474751ee56aabfc0
JTPetter <61797391+JTPetter@users.noreply.github.com>
2023-10-22 23:33:27 +0200

    Template for unit tests

--- tests/testthat/test-variablesChartsSubgroups.R
+++ tests/testthat/test-variablesChartsSubgroups.R
@@ -1,105 +1,179 @@
 context("[Quality Control] Variables Charts for Subgroups")
-
-## Long
-options <- analysisOptions("variablesChartsSubgroups")
-options$measurementLongFormat <- "Diameter"
-options$subgroup <- "Time"
-options$warningLimits <- TRUE
+.numDecimals <- 2
 set.seed(1)
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Long.csv", options)
 
-# R chart
-test_that("X-bar & R Control Chart plot matches", {
-  plotName <- results[["results"]][["XbarPlot"]][["data"]]
+# Long / Column format
+
+## Basic tests (all verified with Minitab)
+
+### x-bar & r chart with manual subgroup size (verified with Minitab)
+options <- analysisOptions("variablesChartsSubgroups")
+options$variablesLong <- "Diameter"
+results <- runAnalysis("variablesChartsSubgroups",
+                       "datasets/variableChartsSubgroups/variableChartsSubgroupsLongFormat.csv",
+                       options)
+
+test_that("X-bar & R control chart plot matches", {
+ plotName <- results[["results"]][["controlCharts"]][["collection"]][["controlCharts_plot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   jaspTools::expect_equal_plots(testPlot, "x-bar-r-control-chart")
 })
 
+### x-bar & s chart with manual subgroup size (verified with Minitab) Note: JASP does not use the unbiasing constant.
+options <- analysisOptions("variablesChartsSubgroups")
+options$variablesLong <- "Diameter"
+options$TypeChart <- "xBarSchart"
+results <- runAnalysis("variablesChartsSubgroups",
+                       "datasets/variableChartsSubgroups/variableChartsSubgroupsLongFormat.csv",
+                       options)
 
-# S chart
-options$chartType <- "xBarAndS"
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Long.csv", options)
-
-test_that("X-bar & s Control Chart plot matches", {
-  plotName <- results[["results"]][["SPlot"]][["data"]]
+test_that("X-bar & s control chart plot matches", {
+plotName <- results[["results"]][["controlCharts"]][["collection"]][["controlCharts_plot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   jaspTools::expect_equal_plots(testPlot, "x-bar-s-control-chart")
 })
 
-## Wide
-options$dataFormat <- "wideFormat"
-options$chartType <- "xBarAndR"
-options$measurementsWideFormat <- c("dm1", "dm2", "dm3", "dm4", "dm5")
-options$axisLabels <- "Time"
-set.seed(1)
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Wide.csv", options)
+### x-bar & r chart with manual subgroup size and stages
 
-test_that("X-bar & R Control Chart2 plot matches", {
-  plotName <- results[["results"]][["XbarPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "x-bar-r-control-chart2")
-})
+### x-bar & s chart with manual subgroup size and stages
 
-# S chart
-options$chartType <- "xBarAndS"
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Wide.csv", options)
+### x-bar & r chart with subgroup variable
 
-test_that("X-bar & s Control Chart 2 plot matches", {
-  plotName <- results[["results"]][["SPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "x-bar-s-control-chart2")
-})
+### x-bar & s chart with subgroup variable
 
+### x-bar & r chart with subgroup variable and stages
 
-### Unequal subgroup sizes
-options <- analysisOptions("variablesChartsSubgroups")
-options$measurementLongFormat <- "Diameter"
-options$subgroupSizeType <- "manual"
-options$manualSubgroupSizeValue <- 22
-options$subgroupSizeUnequal <- "actualSizes"
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Long.csv", options)
+### x-bar & s chart with subgroup variable and stages
 
-test_that("X-bar & R Control Chart3 plot matches", {
-  plotName <- results[["results"]][["XbarPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "x-bar-r-control-chart3")
-})
+### x-bar & r chart with warning limits
+
+### x-bar & s chart with warning limits
+
+### x-bar & r chart with known parameters
+
+### x-bar & s chart with known parameters
+
+### Change manual subgroup size value
+
+### Report function with x-bar & r chart
+
+### Report function with x-bar & s chart
 
 
-### Known parameters x-bar & r-chart
-options <- analysisOptions("variablesChartsSubgroups")
-options$measurementLongFormat <- "Diameter"
-options$subgroupSizeType <- "manual"
-options$manualSubgroupSizeValue <- 22
-options$subgroupSizeUnequal <- "actualSizes"
-options$knownParameters <- TRUE
-options$knownParametersMean <- -6
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Long.csv", options)
+## Missing values handling
 
-test_that("Test results for R chart table results match", {
-  table <- results[["results"]][["NelsonTables"]][["collection"]][["NelsonTables_NelsonTableR"]][["data"]]
-  jaspTools::expect_equal_tables(table,
-                                 list(5))
-})
+### Missing values in measurements variable
 
-test_that("X-bar & R Control Chart 4 plot matches", {
-  plotName <- results[["results"]][["XbarPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "x-bar-r-control-chart4")
-})
+#### Single missing value
 
-### Known parameters x-bar & s-chart
-options$chartType <- "xBarAndS"
-results <- runAnalysis("variablesChartsSubgroups", "SPCSubgroups_Long.csv", options)
+##### x-bar & r chart
 
-test_that("Test results for s chart table results match", {
-  table <- results[["results"]][["NelsonTables"]][["collection"]][["NelsonTables_NelsonTableS"]][["data"]]
-  jaspTools::expect_equal_tables(table,
-                                 list(1, 2, 3, 4, 5))
-})
+##### x-bar & s chart
 
-test_that("X-bar & s Control Chart 3 plot matches", {
-  plotName <- results[["results"]][["SPlot"]][["data"]]
-  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "x-bar-s-control-chart3")
-})
+#### All but one value missing
+
+##### x-bar & r chart
+
+##### x-bar & s chart
+
+#### All values missing
+
+##### x-bar & r chart
+
+##### x-bar & s chart
+
+### Missing values in subgroup variable
+
+#### x-bar & r chart
+
+#### x-bar & s chart
+
+### Missing values in stages variable
+
+#### x-bar & r chart
+
+#### x-bar & s chart
+
+
+## Unequal subgroup sizes
+
+### x-bar & r chart with actual sizes
+
+### x-bar & s chart with actual sizes
+
+### x-bar & r chart with fixed group calculation
+
+### x-bar & s chart with fixed group calculation
+
+
+## Edge cases
+
+### Subgroup size larger than data or stage
+
+### Multiple stages assigned within subgroup
+
+
+
+# Wide / Row format
+
+## Basic tests
+
+### x-bar & r chart
+
+### x-bar & s chart
+
+### x-bar & r chart with stages
+
+### x-bar & s chart with stages
+
+### x-bar & r chart with warning limits
+
+### x-bar & s chart with warning limits
+
+### x-bar & r chart with known parameters
+
+### x-bar & s chart with known parameters
+
+### Report function with x-bar & r chart
+
+### Report function with x-bar & s chart
+
+
+## Missing values handling
+
+### Missing values in measurements variable
+
+#### Single missing value
+
+##### x-bar & r chart
+
+##### x-bar & s chart
+
+#### All but one value missing
+
+##### x-bar & r chart
+
+##### x-bar & s chart
+
+#### All values missing
+
+##### x-bar & r chart
+
+##### x-bar & s chart
+
+### Missing values in stages variable
+
+#### x-bar & r chart
+
+#### x-bar & s chart
+
+
+## Unequal subgroup sizes
+
+### x-bar & r chart with actual sizes
+
+### x-bar & s chart with actual sizes
+
+### x-bar & r chart with fixed group calculation
+
+### x-bar & s chart with fixed group calculation
