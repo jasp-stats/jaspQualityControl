310b190cccee14b30463f99680006ce7cc4d60cd
Don van den Bergh <donvdbergh@hotmail.com>
2025-07-01 08:51:54 +0200

    add interval table

--- R/bayesianProcessCapabilityStudies.R
+++ R/bayesianProcessCapabilityStudies.R
@@ -22,6 +22,7 @@ bayesianProcessCapabilityStudies <- function(jaspResults, dataset, options) {
 
   fit <- .bpcsCapabilityTable(jaspResults, dataset, options)
   .bpcsCapabilityPlot(jaspResults, options, fit)
+  .bpcsIntervalTable(jaspResults, options, fit)
 
 }
 
@@ -126,6 +127,57 @@ bayesianProcessCapabilityStudies <- function(jaspResults, dataset, options) {
 
 }
 
+.bpcsIntervalTable <- function(jaspResults, options, fit) {
+
+  if (!options[["intervalTable"]])
+    return()
+
+  table <- .bpcsIntervalTableMeta(jaspResults, options)
+  if (!.bpcsIsReady(options) || is.null(fit))
+    return()
+
+  tryCatch({
+
+    # qc does c(-Inf, interval_probability, Inf)
+    interval_probability <- unlist(options[paste0("interval", 1:5)], use.names = FALSE)
+    interval_summary <- summary(fit[["rawfit"]], interval_probability = interval_probability)[["interval_summary"]]
+    colnames(interval_summary) <- paste0("interval", 1:6)
+    table$setData(interval_summary)
+
+  }, error = function(e) {
+
+    table$setError(gettextf("Unexpected error in interval table: %s", e$message))
+
+  })
+
+  return()
+}
+
+.bpcsIntervalTableMeta <- function(jaspResults, options) {
+
+  table <- createJaspTable(title = gettext("Interval Table"))
+
+  table$addColumnInfo(name = "metric",  title = gettext("Capability\nMeasure"), type = "string")
+
+  intervalBounds <- c(-Inf, unlist(options[paste0("interval", 1:5)], use.names = FALSE), Inf)
+  n <- length(intervalBounds)
+  for (i in 1:(n - 1)) {
+    j <- i + 1
+    lhs <- if (i == 1)     "(" else "["
+    rhs <- if (i == n - 1) ")" else "]"
+    title <- sprintf("%s%.3f ,  %.3f%s", lhs, intervalBounds[i], intervalBounds[j], rhs)
+    table$addColumnInfo(name = paste0("interval", i), title = title, type = "number")
+  }
+  table$dependOn(c(
+    "measurementLongFormat", "intervalTable", "credibleIntervalWidth",
+    paste0("interval", 1:5), "targetValue", "lowerSpecificationLimitValue", "upperSpecificationLimitValue"
+  ))
+
+  jaspResults[["bpcsIntervalTable"]] <- table
+  return(table)
+}
+
+
 # Plots ----
 .bpcsCapabilityPlot <- function(jaspResults, options, fit) {
 
@@ -136,7 +188,8 @@ bayesianProcessCapabilityStudies <- function(jaspResults, dataset, options) {
     createJaspPlot(
       title = gettext("Posterior Distribution"),
       plot  = if (.bpcsIsReady(options) && !is.null(fit)) {
-        qc::plot_density(fit$summaryObject) +
+        qc::plot_density(fit$summaryObject,
+                         what = c("Cp", "CpU", "CpL", "Cpk", "Cpm")) +
           jaspGraphs::geom_rangeframe() +
           jaspGraphs::themeJaspRaw()
       } else {
